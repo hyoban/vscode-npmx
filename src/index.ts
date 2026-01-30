9ed4331e4533707f9979b4840e54620cd6315fa0
Vida Xie <vida_2020@163.com>
2026-01-31 02:47:32 +0800

    fix: move versionCompletion check before provider registration

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import { defineExtension } from 'reactive-vscode'
 import { displayName, version } from './generated-meta'
 import { registerJsonCompletionItemProvider } from './providers/completion-item/json'
 import { registerJsonDocumentLinkProvider } from './providers/document-link/json'
-import { logger } from './state'
+import { config, logger } from './state'
 
 export const { activate, deactivate } = defineExtension((ctx) => {
   logger.info(`${displayName} Activated, v${version}`)
 
   ctx.subscriptions.push(
     registerJsonDocumentLinkProvider(),
-    registerJsonCompletionItemProvider(),
   )
+
+  if (config.versionCompletion !== 'off') {
+    ctx.subscriptions.push(
+      registerJsonCompletionItemProvider(),
+    )
+  }
 })
--- src/providers/completion-item/json.ts
+++ src/providers/completion-item/json.ts
@@ -1,7 +1,6 @@
 import type { Node } from 'jsonc-parser'
 import type { Position, TextDocument } from 'vscode'
 import { PACKAGE_JSON_PATTERN } from '#constants'
-import { config } from '#state'
 import { getNodeRange, isInDepSection, parse } from '#utils/ast/json'
 import { findNodeAtOffset } from 'jsonc-parser'
 import { languages } from 'vscode'
@@ -32,8 +31,6 @@ class JsonCompletionItemProvider extends BaseCompletionItemProvider<Node> {
 }
 
 export function registerJsonCompletionItemProvider() {
-  if (config.versionCompletion === 'off')
-    return
   return languages.registerCompletionItemProvider(
     { pattern: PACKAGE_JSON_PATTERN },
     new JsonCompletionItemProvider(),

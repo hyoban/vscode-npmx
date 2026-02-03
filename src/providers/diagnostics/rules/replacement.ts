9a7ad7ea3c9413bfc2e0741bb12f31a885140fdc
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-02-03 23:27:59 +0800

    update

--- src/providers/diagnostics/rules/replacement.ts
+++ src/providers/diagnostics/rules/replacement.ts
@@ -3,7 +3,6 @@ import type { DiagnosticRule } from '..'
 import { getReplacement } from '#utils/api/replacement'
 import { DiagnosticSeverity } from 'vscode'
 
-// https://github.com/es-tooling/eslint-plugin-depend/blob/5f81ab8b0a0a48f2b528dd47a78964e533b5a699/src/util/rule-meta.ts#L10-L26
 function getMdnUrl(path: string): string {
   return `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/${path}`
 }
@@ -12,21 +11,17 @@ function getReplacementsDocUrl(path: string): string {
   return `https://github.com/es-tooling/module-replacements/blob/main/docs/modules/${path}.md`
 }
 
-// https://github.com/es-tooling/eslint-plugin-depend/blob/5f81ab8b0a0a48f2b528dd47a78964e533b5a699/src/rules/ban-dependencies.ts#L59-L69
 // https://github.com/npmx-dev/npmx.dev/blob/main/app/components/PackageReplacement.vue#L8-L30
 function generateMessage(replacement: ModuleReplacement) {
   switch (replacement.type) {
     case 'native':
-      return `${replacement.moduleName} should be replaced with native functionality. `
-        + `You can instead use ${replacement.replacement}. Read more here: ${getMdnUrl(replacement.mdnPath)}`
+      return `This can be replaced with ${replacement.replacement}, available since Node ${replacement.nodeVersion}. Read more here: ${getMdnUrl(replacement.mdnPath)}`
     case 'simple':
-      return `${replacement.moduleName} should be replaced with inline/local logic. `
-        + `You can instead use ${replacement.replacement}.`
+      return `The community has flagged this package as redundant, with the advice: ${replacement.replacement}.`
     case 'documented':
-      return `${replacement.moduleName} should be replaced with an alternative package. `
-        + `Read more here: ${getReplacementsDocUrl(replacement.docPath)}`
+      return `The community has flagged this package as having more performant alternatives. Read more here: ${getReplacementsDocUrl(replacement.docPath)}`
     case 'none':
-      return `${replacement.moduleName} is a banned dependency. An alternative should be used.`
+      return 'This package has been flagged as no longer needed, and its functionality is likely available natively in all engines.'
   }
 }
 

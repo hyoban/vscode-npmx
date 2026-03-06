19a95f43747a286b8072f11afe833c14b1c008ac
Bowen Hu <62373365+RYGRIT@users.noreply.github.com>
2026-03-07 01:26:20 +0800

    fix: resolve package versions with latest tag cap (#66)
    * ffix: resolve package versions with latest tag cap
    
    * test: remove link + hover unit test
    
    * fix: apply coderabbitai suggestion
    
    * chore: update test description
    
    * update
    
    * test: improve
    
    ---------
    
    Co-authored-by: Vida Xie <vida_2020@163.com>

--- tests/utils/package.test.ts
+++ tests/utils/package.test.ts
@@ -1,5 +1,6 @@
+import type { PackageInfo } from '#utils/api/package'
 import { describe, expect, it } from 'vitest'
-import { encodePackageName } from '../../src/utils/package'
+import { encodePackageName, resolveExactVersion } from '../../src/utils/package'
 
 describe('encodePackageName', () => {
   it('should encode regular package name', () => {
@@ -8,5 +9,36 @@ describe('encodePackageName', () => {
 
   it('should encode scoped package name', () => {
     expect(encodePackageName('@vue/core')).toBe('@vue%2Fcore')
+  })
+})
+
+describe('resolveExactVersion', () => {
+  it.each([
+    ['', '4.10.0'],
+    ['*', '4.10.0'],
+    ['^3.0.0', '3.1.0'],
+    ['^4.0.0', '4.10.0'],
+    ['^5.0.0', null],
+    ['latest', '4.10.0'],
+    ['next', '4.11.0-beta.1'],
+    ['beta', null],
+  ])('should resolve $0 to $1', (spec, version) => {
+    const pkg = {
+      distTags: {
+        latest: '4.10.0',
+        next: '4.11.0-beta.1',
+      },
+      versionsMeta: {
+        '3.0.0': {},
+        '3.1.0': {},
+        '4.10.0': {},
+        '4.10.1': {
+          deprecated: 'Unplanned Release',
+        },
+        '4.11.0-beta.1': {},
+      },
+    } as unknown as PackageInfo
+
+    expect(resolveExactVersion(pkg, spec)).toBe(version)
   })
 })
